6bd1904bb6ddef67b9003e8de8af7424efb38666
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2022-08-30 00:06:45 -0300

    Aula 1

---
+++ src/screens/home/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    background: red;
+`
+
+export const Title = styled.Text`
+    color: yellow
+`
+
+
